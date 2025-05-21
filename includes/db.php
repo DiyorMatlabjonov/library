9962d1e09f113d703a02bdc9ff399310816cb3f5
Diyor <diyorbekmatlabjonov@gmail.com>
2025-05-21 17:21:39 +0300

    Подключение к БД

--- includes/db.php
+++ includes/db.php
@@ -1,2 +1,13 @@
 <?php
-// $file
+$host = 'localhost';
+$db = 'online_library';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Ошибка подключения: " . $e->getMessage());
+}
+?>
