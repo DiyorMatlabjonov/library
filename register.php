40221f779cc621b61231db9ea6a5f4223a8d3e89
Diyor <diyorbekmatlabjonov@gmail.com>
2025-05-21 17:23:02 +0300

    Регистрация

--- register.php
+++ register.php
@@ -1,2 +1,22 @@
 <?php
-// $file
+require 'includes/db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
+    $stmt->execute([$name, $email, $password]);
+
+    header("Location: login.php");
+}
+?>
+
+<!-- HTML-форма -->
+<form method="POST">
+    <input name="name" placeholder="Имя">
+    <input name="email" type="email" placeholder="Email">
+    <input name="password" type="password" placeholder="Пароль">
+    <button type="submit">Зарегистрироваться</button>
+</form>
